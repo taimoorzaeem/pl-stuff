386c0d283ae6d765845e2d2935a96691f07f1a5c
Taimoor Zaeem <taimoorzaeem@gmail.com>
2025-03-16 17:52:30 +0500

    Update minikanren.rkt
    Add some source code from the book The Reasoned Schemer

--- minikanren.rkt
+++ minikanren.rkt
@@ -1,12 +1,40 @@
-;; Took it from: https://stackoverflow.com/a/65392420/16623126
-
 #lang racket
-;; install with: raco pkg install minikanren
 (require minikanren)
 
-(define succeed (== #t #t)) ;; #s is written as succeed
-(define fail (== #f #t)) ;; #u is written as fail
+;; suggested as per the footnote in frame 6 of Chapter 1, #s is written "succeed"
+(define succeed (== #t #t)) ;; #s is written succeed
 
+(define fail (== #f #t)) ;; and #u is written fail
 
-;; check by running
-(run* (q) succeed) ;; evaluates to '(_.0)
+(define caro
+  (lambda (p a)
+    (fresh (d)
+      (== (cons a d) p))))
+
+(define cdro
+  (lambda (p d)
+    (fresh (a)
+      (== (cons a d) p))))
+
+(define conso
+  (lambda (a d p)
+    (== (cons a d) p)))
+
+(define nullo
+  (lambda (x)
+    (== '() x)))
+
+(define pairo
+  (lambda (p)
+    (fresh (a d)
+      (conso a d p))))
+
+(define listo
+  (lambda (l)
+    (conde
+      ((nullo l) succeed)
+      ((pairo l)
+        (fresh (d)
+          (cdro l d)
+          (listo d)))
+      (else fail))))
