3d96334c09bc5caa77efb28c0e0b043bf3678240
Taimoor Zaeem <taimoorzaeem@gmail.com>
2025-07-05 14:19:27 +0500

    prover: add theorems from chap 02

--- scheme/the-little-prover.scm
+++ scheme/the-little-prover.scm
@@ -54,3 +54,53 @@
 
 (dethm equal-same (x) (equal (equal x x) 't))
 (dethm equal-swap (x y) (equal (equal x y) (equal y x)))
+
+;; The Axioms of If (initial)
+
+(dethm if-true (x y) (equal (iff 't x y) x))
+(dethm if-false (x y) (equal (iff 'nil x y) y))
+(dethm if-same (x y) (equal (iff x y y) y))
+
+;; The Axioms of Equal (final)
+
+(dethm equal-if (x y) (iff (equal x y) (equal x y) 't))
+
+;; Example theorem used for rewriting examples
+(dethm jabberwocky (x)
+  (iff (brillig x)
+    (iff (slithy x)
+    (equal (mimsy x) 'borogove)
+    (equal (mome x) 'rath))
+  (iff (uffish x)
+    (equal (frumious x) 'bandersnatch)
+    (equal (frabjous x) 'beamish))))
+
+;; The Axioms of Cons (final)
+
+(dethm cons/car+cdr (x) (iff (atom x) 't (equal (cons (car x) (cdr x)) x)))
+
+;; The Axioms of If (final)
+
+(dethm if-nest-A (x y z) (iff x (equal (iff x y z) y) 't))
+(dethm if-nest-E (x y z) (iff x 't (equal (iff x y z) z)))
+
+;; Example:
+;; We can use the if-next axioms to simplify the following expression:
+
+;; (cons 'statement
+;;   (cons
+;;     (if (equal a 'question)
+;;       (cons n '(answer))
+;;       (cons n '(else)))
+;;     (if (equal a 'question)
+;;       (cons n '(other answer))
+;;       (cons n '(other else)))))
+
+;; into:
+
+;; (cons 'statement
+;;   (if (equal a 'question)
+;;     (cons (cons n '(answer))
+;;       (cons n '(other answer)))
+;;     (cons (cons n '(else))
+;;       (cons n '(other else)))))
