164d8d68b0e5e91994c1a4cb168ac8bf23500c63
Taimoor Zaeem <taimoorzaeem@gmail.com>
2025-07-21 11:12:32 +0500

    typer: add definitions and functions from chap 06

--- racket/the-little-typer.rkt
+++ racket/the-little-typer.rkt
@@ -576,3 +576,127 @@
       (rec-List start
         end
         (step-append E)))))
+
+
+;; Chap 06
+;; =======
+
+;; The Law of Vec
+;; ==============
+;; If E is a type and k is a Nat, then (Vec E k)
+;; is a type.
+
+
+;; The Law of vecnil
+;; =================
+;; vecnil is a (Vec E zero).
+
+
+;; The Law of vec::
+;; ================
+;; If e is an E and es is a (Vec E k),
+;; then (vec:: e es) is a (Vec E (add1 k)).
+
+
+(claim first-of-one
+  (Pi ((E U))
+    (-> (Vec E 1)
+      E)))
+(define first-of-one
+  (lambda (E)
+    (lambda (es)
+      (head es))))
+
+;; That's no good, we need a general first function. Here it is:
+
+(claim first
+  (Pi ((E U)
+       (l Nat))
+    (-> (Vec E (add1 l))
+      E)))
+
+
+;; The Law of Pi
+;; =============
+;; The expression (Pi ((y Y)) X) is a type when Y is
+;; a type, and X is a type if y is a Y.
+
+(define first
+  (lambda (E l)
+    (lambda (es)
+      (head es))))
+
+
+;; Use a More Specific Type
+;; ========================
+;; Make a function total by using a more specific type to
+;; rule out unwanted arguments.
+
+
+;; The previous definition could also have
+;; been written like this:
+;;
+;; (claim ﬁirst
+;;   (Pi ((E U))
+;;     (Pi ((l Nat))
+;;       (-> (Vec E (add1 l))
+;;         E))))
+;; (define first
+;;   (lambda (E)
+;;     (lambda (l)
+;;       (lambda (es)
+;;         (head es)))))
+
+;; or this:
+
+;; (claim ﬁirst
+;;   (Pi ((E U))
+;;     (Pi ((l Nat))
+;;       (Pi ((es (Vec E (add1 l))))
+;;         E))))
+;; (define first
+;;   (lambda (E)
+;;     (lambda (l)
+;;       (lambda (es)
+;;         (head es )))))
+
+
+;; -> and Pi
+;; =========
+;; The type (→ Y X) is a shorter way of writing
+;; (Pi ((y Y)) X) when y is not used in X.
+
+
+;; The Final Law of lambda
+;; =======================
+;; If x is an X when y is a Y, then (lambda (y) x)
+;; is a (Pi ((y Y)) X).
+
+
+;; The Final Law of Application
+;; ============================
+;; If f is a (Pi ((y Y)) X) and z is a Y, then (f z) is an
+;; X where every y has been consistently replaced by z.
+
+
+;; The Final First Commandment of lambda
+;; =====================================
+;; If two λ-expressions can be made the same (Pi ((y Y)) X),
+;; by consistently renaming their variables, then they are the same.
+
+
+;; The Final Second Commandment of lambda
+;; ======================================
+;; If f is a (Pi ((y Y)) X), and y does not occur in
+;; f then f is the same as (lambda (y) (f y)).
+
+
+(claim rest
+  (Pi ((E U)
+       (l Nat))
+    (-> (Vec E (add1 l))
+      (Vec E l))))
+(define rest
+  (lambda (E l)
+    (lambda (es)
+      (tail es))))
