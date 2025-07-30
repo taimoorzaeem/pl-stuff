1c24b083a5d38ab8171e2bbbbe84b6e13fc4152f
Taimoor Zaeem <taimoorzaeem@gmail.com>
2025-07-30 19:17:36 +0500

    typer: add definitions and functions from chap 10
    Signed-off-by: Taimoor Zaeem <taimoorzaeem@gmail.com>

--- racket/the-little-typer.rkt
+++ racket/the-little-typer.rkt
@@ -1224,3 +1224,136 @@
 ;; =======================
 ;; If x is an X, then (symm (same x)) is the same (= X x x)
 ;; as (same x).
+
+
+;; Chap 10
+;; =======
+
+;; The Law of Sigma
+;; ================
+;; The expression (Sigma ((x A)) D) is a type when A is a type,
+;; and D is a type if x is an A.
+
+;; NOTE: It is sometimes referred as "D is a family of types over A".
+
+;; The Commandment of cons
+;; =======================
+;; If p is a (Sigma ((x A)) D), then p is the same as
+;; (cons (car p) (cdr p)).
+
+
+;; NOTE: (Pair A D) is a short way of writing (Sigma ((x A)) D)
+;;       where x is not used in D.
+
+;; NOTE: Types built with ->, Pi, and = can be read as statements, and
+;;       expressions of those types are PROOFS. Similarly, types built
+;;       with Pair and Sigma can be read as statements.
+
+;; Use a Specific Type for Correctness
+;; ===================================
+;; Specific types can rule out foolish definitions.
+;; NOTE: I wouldn't call them foolish, they are just not correct enough.
+
+
+(claim replicate
+  (Pi ((E U)
+       (l Nat))
+    (-> E
+      (Vec E l))))
+
+
+(claim mot-replicate
+  (-> U Nat
+    U))
+(define mot-replicate
+  (lambda (E k)
+    (Vec E k)))
+
+
+(claim step-replicate
+  (Pi ((E U)
+       (e E)
+       (l-1 Nat))
+    (-> (mot-replicate E l-1)
+      (mot-replicate E (add1 l-1)))))
+(define step-replicate
+  (lambda (E e l-1)
+    (lambda (step-replicate_l-1)
+      (vec:: e step-replicate_l-1))))
+
+
+(define replicate
+  (lambda (E l)
+    (lambda (e)
+      (ind-Nat l
+        (mot-replicate E)
+        vecnil
+        (step-replicate E e)))))
+
+
+(claim list->vec
+  (Pi ((E U)
+       (es (List E)))
+    (Vec E (length E es))))
+
+
+;; The Law of ind-List
+;; ===================
+;; If target is a (List E), mot is an (-> (List E) U),
+;; base is a (mot nil), and step is a
+;;   (Pi ((e E)
+;;        (es (List E)))
+;;     (-> (mot es )
+;;       (mot (:: e es ))))
+;; then (ind-List target mot base step) is a (mot target).
+
+
+;; The First Commandment of ind-List
+;; =================================
+;; The ind-List-expression (ind-List nil mot base step)
+;; is the same (mot nil) as base.
+
+
+;; The Second Commandment of ind-List
+;; ==================================
+;; The ind-List-expression
+;; (ind-List (:: e es)
+;;   mot
+;;   base
+;;   step)
+;; is the same (mot (:: e es)) as
+;;   (step e es
+;;     (ind-List es
+;;       mot
+;;       base
+;;       step)).
+
+
+(claim mot-list->vec
+  (Pi ((E U))
+    (-> (List E)
+      U)))
+(define mot-list->vec
+  (lambda (E)
+    (lambda (es)
+      (Vec E (length E es)))))
+
+
+(claim step-list->vec
+  (Pi ((E U)
+       (e E)
+       (es (List E)))
+    (-> (mot-list->vec E es)
+      (mot-list->vec E (:: e es)))))
+(define step-list->vec
+  (lambda (E e es)
+    (lambda (list->vec_es)
+      (vec:: e list->vec_es))))
+
+
+(define list->vec
+  (lambda (E es)
+    (ind-List es
+      (mot-list->vec E)
+      vecnil
+      (step-list->vec E))))
