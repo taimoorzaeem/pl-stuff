31ff18f10350397c46e55fce684c6f9e46b53198
Taimoor Zaeem <taimoorzaeem@gmail.com>
2025-08-12 20:19:20 +0500

    typer: add definitions and functions from chap 16

--- racket/the-little-typer.rkt
+++ racket/the-little-typer.rkt
@@ -2113,3 +2113,97 @@
     (Either X
       (-> X
         Absurd))))
+
+
+;; Chap 16
+;; =======
+
+;; NOTE: Commented out because 'zero-not-add1' is not defined.
+;; (claim zero?
+;;   (Pi ((j Nat))
+;;     (Dec
+;;       (= Nat zero j))))
+;; (define zero?
+;;   (lambda (j)
+;;     (ind-Nat j
+;;       (lambda (k)
+;;         (Dec (= Nat zero k)))
+;;       (left (same zero))
+;;       (lambda (j-1 zero?_n-1)
+;;         (right
+;;           (zero-not-add1 j-1))))))
+
+
+;; (claim nat=?
+;;   (Pi ((n Nat)
+;;        (j Nat))
+;;     (Dec
+;;       (= Nat n j))))
+;; 
+;; 
+;; (claim mot-nat=?
+;;   (-> Nat
+;;     U))
+;; (define mot-nat=?
+;;   (lambda (k)
+;;     (Pi ((j Nat))
+;;       (Dec
+;;         (= Nat k j)))))
+;; 
+;; 
+;; (claim step-nat=?
+;;   (Pi ((n-1 Nat))
+;;     (-> (mot-nat=? n-1)
+;;       (mot-nat=? (add1 n-1)))))
+;; 
+;; 
+;; (claim add1-not-zero
+;;   (Pi ((n Nat))
+;;     (-> (= Nat (add1 n) zero)
+;;       Absurd)))
+;; (define add1-not-zero
+;;   (lambda (n)
+;;     (use-Nat= (add1 n) zero)))
+;; 
+;; 
+;; (claim dec-add1=
+;;   (Pi ((n-1 Nat)
+;;        (j-1 Nat))
+;;     (-> (Dec (= Nat n-1 j-1))
+;;       (Dec (= Nat (add1 n-1) (add1 j-1))))))
+;; (define dec-add1=
+;;   (lambda (n-1 j-1 eq-or-not)
+;;     (ind-Either eq-or-not
+;;       (lambda (target)
+;;         (Dec
+;;           (= Nat (add1 n-1) (add1 j-1))))
+;;       (lambda (yes)
+;;         (left
+;;           (cong yes (+ 1))))
+;;       (lambda (no)
+;;         (right
+;;           (lambda (n=j)
+;;             (no
+;;               (sub1= n-1 j-1
+;;                 n=j))))))))
+;; 
+;; 
+;; (define step-nat=?
+;;   (lambda (n-1 nat=?_n-1 j)
+;;     (ind-Nat j
+;;       (lambda (k)
+;;         (Dec (= Nat (add1 n-1) k)))
+;;       (right
+;;         (add1-not-zero n-1))
+;;       (lambda (j-1 nat=?_n-1)
+;;         (dec-add1= n-1 j-1
+;;           (nat=?_n-1 j-1))))))
+;; 
+;; 
+;; (define nat=?
+;;   (lambda (n j)
+;;     ((ind-Nat n
+;;       mot-nat=?
+;;       zero?
+;;       step-nat=?)
+;;     j)))
